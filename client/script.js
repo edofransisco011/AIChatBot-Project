8a353ed8f3d48d28507d71d916237b84dd0170ab
edofransisco011 <edofransisco011@gmail.com>
2023-05-26 16:01:17 +0700

    Commit update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:8080', {
+    const response = await fetch('http://localhost:3080', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
