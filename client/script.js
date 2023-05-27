bc855a21b0854c7117b791f17c4f179e6586cc57
edofransisco011 <edofransisco011@gmail.com>
2023-05-27 10:05:06 +0700

    Client update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3080', {
+    const response = await fetch('https://react-chatgpt-rho.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
